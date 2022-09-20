5fe9a8b028afb7fcb8baace7e4685e46bbd242de
Grigory Vakulsky <g.vakulsky@gmail.com>
2022-09-20 11:42:55 +0300

    Working archive tool with custom signature

--- main.cpp
+++ main.cpp
@@ -6,19 +6,15 @@
 #include<cstring>
 #include <ctime>
 
+const int PD = 128;
+const char* signature = "ZGV";
 
 struct tar_header{
     char signature[8];
-    char name[100];
-    char mode[8];
-    char owner[8];
-    char group[8];
+    char name[50];
+    char version[8];
     char size[12];
-    char modified[12];
-    char checksum[8];
-    char type[1];
-    char link[100];
-    char padding[255];
+    char padding[22];
 };
 
 void fexpand(FILE* f, size_t amount, int value){
@@ -30,9 +26,9 @@ void fexpand(FILE* f, size_t amount, int value){
 void tar_add(FILE* tar_file, const char* file, const char* internal_name){
     //Get current position; round to a multiple of 512 if we aren't there already
     size_t index = ftell( tar_file );
-    size_t offset = index % 512;
+    size_t offset = index % PD;
     if( offset != 0 ){
-        fexpand( tar_file, 512 - offset, 0);
+        fexpand( tar_file, PD - offset, 0);
     }
     //Store the index for the header to return to later
     index = ftell( tar_file );
@@ -53,31 +49,23 @@ void tar_add(FILE* tar_file, const char* file, const char* internal_name){
     //Get the end to calculate the size of the file
     size_t end = ftell( tar_file );
     //Round the file size to a multiple of 512 bytes
-    offset = end % 512;
+    offset = end % PD;
     if( end != 0 ){
-        fexpand( tar_file, 512 - offset, 0);
+        fexpand( tar_file, PD - offset, 0);
     }
     //Fill out a new tar header
-    struct tar_header header;
+    struct tar_header header{};
     memset( &header, 0, sizeof( struct tar_header ) );
+    snprintf( header.signature, 8, "%s", signature  );
     snprintf( header.name, 100, "%s", internal_name  );
-    snprintf( header.mode, 8, "%06o ", 0777 ); //You should probably query the input file for this info
-    snprintf( header.owner, 8, "%06o ", 0 ); //^
-    snprintf( header.group, 8, "%06o ", 0 ); //^
-    snprintf( header.size, 12, "%011o", end - 512 - index );
-    snprintf( header.modified, 12, "%011o ", time(0) ); //Again, get this from the filesystem
-    memset( header.checksum, ' ', 8);
-    header.type[0] = '0';
+    snprintf( header.version, 8, "%07o", 1 );
+    snprintf( header.size, 12, "%011llo", end - PD - index );
+
 
     //Calculate the checksum
     size_t checksum = 0;
     int i;
     const auto* bytes = reinterpret_cast<const unsigned char *>(&header);
-    for( i = 0; i < sizeof( struct tar_header ); ++i ){
-        checksum += bytes[i];
-    }
-
-    snprintf( header.checksum, 8, "%06o ", checksum );
 
     //Save the new end to return to after writing the header
     end = ftell(tar_file);
